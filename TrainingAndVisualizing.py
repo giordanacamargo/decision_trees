a82ccade27aa4b6576085445817a7e773d3ebd83
Giordana Camargo <giordanacamilla@gmail.com>
2023-06-18 18:22:39 -0300

    Refatoração de código

--- TrainingAndVisualizing.py
+++ TrainingAndVisualizing.py
@@ -1,34 +1,9 @@
-import sys
-
-assert sys.version_info >= (3, 7)
-from packaging import version
-import sklearn
-
-assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
-import matplotlib.pyplot as plt
-from pathlib import Path
-from graphviz import Source
+import graphviz
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-import graphviz
-import subprocess
-
-plt.rc('font', size=14)
-plt.rc('axes', labelsize=14, titlesize=14)
-plt.rc('legend', fontsize=14)
-plt.rc('xtick', labelsize=10)
-plt.rc('ytick', labelsize=10)
-
-IMAGES_PATH = Path() / "images" / "decision_trees"
-IMAGES_PATH.mkdir(parents=True, exist_ok=True)
-
-
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
-    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format=fig_extension, dpi=resolution)
+from graphviz import Source
+from Setup import IMAGES_PATH
 
 
 iris = load_iris(as_frame=True)
